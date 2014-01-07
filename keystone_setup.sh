ee1c3e7f39565564cc433860cb0943de098be428
Neependra Khare <neependra.khare@gmail.com>
2014-01-07 07:04:50 +0530

    Updated keystone setup

---
+++ keystone_setup.sh
@@ -0,0 +1,27 @@
+/usr/bin/mysqld_safe  &
+until mysql -e 'select 1'; do sleep 1; done
+openstack-db --init --service keystone
+export IP=$(ip addr | grep inet | grep eth0 | awk -F" " '{print $2}'| sed -e 's/\/.*$//')
+export SERVICE_TOKEN=$(openssl rand -hex 10)
+export SERVICE_ENDPOINT=http://$IP:35357/v2.0
+echo $SERVICE_TOKEN > /tmp/ks_admin_token
+openstack-config --set /etc/keystone/keystone.conf DEFAULT admin_token $SERVICE_TOKEN
+keystone-all &
+keystone service-create --name keystone --type identity --description "Keystone Identity Service"
+KEYSTONE_SERVICE_ID=$(keystone service-list | awk '/\ keystone\ / {print $2}') 
+keystone endpoint-create --service_id $KEYSTONE_SERVICE_ID --publicurl "http://$IP:5000/v2.0" --adminurl "http://$IP:35357/v2.0" --internalurl "http://$IP:5000/v2.0"  
+ADMIN_TOKEN=`cat /tmp/ks_admin_token`
+keystone --token $ADMIN_TOKEN --endpoint http://$IP:35357/v2.0 user-create --name admin --pass redhat
+keystone --token $ADMIN_TOKEN --endpoint http://$IP:35357/v2.0 role-create --name admin
+keystone --token $ADMIN_TOKEN --endpoint http://$IP:35357/v2.0 tenant-create --name admin
+keystone --token $ADMIN_TOKEN --endpoint http://$IP:35357/v2.0 user-role-add --user admin --role admin --tenant admin
+echo "export OS_USERNAME=admin" >> ~/keystonerc_admin
+echo "export OS_TENANT_NAME=admin" >> ~/keystonerc_admin
+echo "export OS_PASSWORD=redhat" >> ~/keystonerc_admin
+echo "export OS_AUTH_URL=http://$IP:35357/v2.0/" >> ~/keystonerc_admin
+echo "export PS1='[\u@\h \W(keystone_admin)]\$ '" >> ~/keystonerc_admin
+source ~/keystonerc_admin
+keystone user-list
+curl -s http://127.0.0.1:35357/v2.0/tokens -X 'POST'  -d '{"auth":{"passwordCredentials":{"username":"admin", "password":"redhat"}}}'  -H "Content-Type: application/json" | python -m json.tool
+curl -s http://127.0.0.1:35357/v2.0/tokens -X 'POST'  -d '{"auth":{"passwordCredentials":{"username":"admin", "password":"redhat1"}}}'  -H "Content-Type: application/json" | python -m json.tool
+wait
--- mysql_startup.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/mysqld_safe  &
-until mysql -e 'select 1'; do sleep 1; done
-wait
