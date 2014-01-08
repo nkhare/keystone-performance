4641f5bf3eb0b5beeac59bde13dc209b0437bbf7
Neependra Khare <neependra.khare@gmail.com>
2014-01-08 11:51:37 +0530

    Updated the docfile and keystone setup script

--- keystone_setup.sh
+++ keystone_setup.sh
@@ -1,13 +1,12 @@
 /usr/bin/mysqld_safe  &
 until mysql -e 'select 1'; do sleep 1; done
 useradd keystone
-openstack-db --init --service keystone -y --rootpw ""
-sleep 20
-keystone-manage pki_setup --keystone-user keystone --keystone-group keystone
 mkdir -p /etc/keystone
 cp -r /opt/keystone/etc/* /etc/keystone/
 cp /opt/keystone/etc/keystone.conf.sample /etc/keystone/keystone.conf
 openstack-config --set /etc/keystone/keystone.conf database connection mysql://root:@localhost/keystone
+openstack-db --init --service keystone -y --rootpw ""
+keystone-manage pki_setup --keystone-user keystone --keystone-group keystone
 export IP=$(ip addr | grep inet | grep eth0 | awk -F" " '{print $2}'| sed -e 's/\/.*$//')
 export SERVICE_TOKEN=$(openssl rand -hex 10)
 export SERVICE_ENDPOINT=http://$IP:35357/v2.0
@@ -17,21 +16,19 @@ keystone-all --config-file /etc/keystone/keystone.conf &
 > ~/keystonerc_admin
 echo "export OS_USERNAME=admin" >> ~/keystonerc_admin
 echo "export OS_TENANT_NAME=admin" >> ~/keystonerc_admin
-echo "export OS_PASSWORD=redhat" >> ~/keystonerc_admin
+echo "export OS_PASSWORD=test" >> ~/keystonerc_admin
 echo "export OS_AUTH_URL=http://$IP:35357/v2.0/" >> ~/keystonerc_admin
 echo "export PS1='[\u@\h \W(keystone_admin)]\$ '" >> ~/keystonerc_admin
 source ~/keystonerc_admin
-sleep 2
 keystone service-create --name keystone --type identity --description "Keystone Identity Service"
 KEYSTONE_SERVICE_ID=$(keystone service-list | awk '/\ keystone\ / {print $2}')
 keystone endpoint-create --service_id $KEYSTONE_SERVICE_ID --publicurl "http://$IP:5000/v2.0" --adminurl "http://$IP:35357/v2.0" --internalurl "http://$IP:5000/v2.0"
 ADMIN_TOKEN=`cat /tmp/ks_admin_token`
-keystone --token $ADMIN_TOKEN --endpoint http://$IP:35357/v2.0 user-create --name admin --pass redhat
+keystone --token $ADMIN_TOKEN --endpoint http://$IP:35357/v2.0 user-create --name admin --pass test
 keystone --token $ADMIN_TOKEN --endpoint http://$IP:35357/v2.0 role-create --name admin
 keystone --token $ADMIN_TOKEN --endpoint http://$IP:35357/v2.0 tenant-create --name admin
 keystone --token $ADMIN_TOKEN --endpoint http://$IP:35357/v2.0 user-role-add --user admin --role admin --tenant admin
 keystone user-list
-curl -s http://127.0.0.1:35357/v2.0/tokens -X 'POST'  -d '{"auth":{"passwordCredentials":{"username":"admin", "password":"redhat"}}}'  -H "Content-Type: application/json" | python -m json.tool
-curl -s http://127.0.0.1:35357/v2.0/tokens -X 'POST'  -d '{"auth":{"passwordCredentials":{"username":"admin", "password":"redhat1"}}}'  -H "Content-Type: application/json" | python -m json.tool
+curl -s http://127.0.0.1:35357/v2.0/tokens -X 'POST'  -d '{"auth":{"passwordCredentials":{"username":"admin", "password":"test"}}}'  -H "Content-Type: application/json" | python -m json.tool
+curl -s http://127.0.0.1:35357/v2.0/tokens -X 'POST'  -d '{"auth":{"passwordCredentials":{"username":"admin", "password":"test1"}}}'  -H "Content-Type: application/json" | python -m json.tool
 wait
-
